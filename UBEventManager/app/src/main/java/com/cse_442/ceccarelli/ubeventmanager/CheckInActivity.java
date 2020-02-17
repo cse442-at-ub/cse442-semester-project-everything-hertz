1f15de27bcd922df03d716bc1a1d23b56f96b0c6
Kat Semenova <katya.semenova1405@gmail.com>
2020-02-16 23:50:50 -0500

    #20 Create a page that will access the camera of the phone

--- UBEventManager/app/src/main/java/com/cse_442/ceccarelli/ubeventmanager/CheckInActivity.java
+++ UBEventManager/app/src/main/java/com/cse_442/ceccarelli/ubeventmanager/CheckInActivity.java
@@ -2,12 +2,21 @@ package com.cse_442.ceccarelli.ubeventmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class CheckInActivity extends AppCompatActivity {
+public class CheckInActivity extends AppCompatActivity  {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
     }
+
 }
