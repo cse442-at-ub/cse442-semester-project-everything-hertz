925fc55235ccddbd12abe40556b7f572a19df1b0
Kat Semenova <katya.semenova1405@gmail.com>
2020-03-09 16:39:20 -0400

    #55 Create query to get total points for a specific user from our database

--- php_files_kat/connection.php
+++ php_files_kat/connection.php
@@ -1,9 +1,9 @@
-<? php
+<?php
 $db_name= "cse442_542_2020_spring_teamk_db";
 $mysql_username = "ksemenov";
 $mysql_password = "50219589";
-$server_name = "tethys.cse.buffalo.edu";
-$connect = mysqli_connect($server_name,$mysql_username,$mysql_password);
+$server_name = "tethys.cse.buffalo.edu:3306";
+$conn = mysqli_connect($server_name,$mysql_username,$mysql_password);
 if($conn){
 	echo "connection success";
 }else{
